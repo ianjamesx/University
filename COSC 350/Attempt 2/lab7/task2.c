5043ba84fa3e77e01b4e5aa38fb76ae275cc9c3b
ian <47257912+ianjamesx@users.noreply.github.com>
2020-04-08 13:47:16 -0400

    Add files via upload

---
+++ COSC 350/Attempt 2/lab7/task2.c
@@ -0,0 +1,34 @@
+/* Catch ^C with sigaction
+ * A. Copy ctrlc1.c from BLP4th edition page 483, 466 in 3rd edition. Run it to see how it behaves.
+ * B. Copy ctrlc2.c from BLP4th edition page 488, 470 in 3rd edition. Run it to see how it behaves. Not that it does not behave exactly like ctrlc1.
+ * C. BLP advises using SIGQUIT (^\) to quit from ctrlc2. Try it to see how it behaves.
+ * D. As an alternative way to quit from it, put it in the background, then issue a kill. Write down a brief description of exactly what you did.
+ * E. Modify your ctrlc2.c so it behaves exactly the same as ctrl1.c Hint: read the sigaction Flags section of BLP, Chapter 11.
+ * F. Print your modified ctrlc2.c to hand in.
+ * G. While we're at it, take a look at what is printed by kill -l. It's an easy way to get a list of all the signal numbers and names.
+ */
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void ouch(int sig){
+  printf("\nOUCH - I got a signal %d\n", sig);
+}
+
+int main(){
+  struct sigaction act;
+
+  act.sa_handler = ouch;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags=SA_RESETHAND;
+
+  sigaction(SIGINT, &act,0);
+
+  while(1){
+    printf("Hello world!\n");
+    sleep(1);
+  }
+  
+  return 0;
+}
---
+++ COSC 350/Attempt 2/lab7/task3.c
@@ -0,0 +1,37 @@
+/* Process communication without sending signal.
+ * Write a complete C program in which a child process writes a message "Hi, Mom" to a file named foo.
+ * The parent process reads the message and prints it to standard output "My son said Hi Mom". The part of the message "Hi Mom" is retrieved from the file foo. Assume that all system calls succeed (no need to error check). Use only low-level file operations(fork, wait, open, close, read, write, lseek). You must make sure a child process terminates first
+ */
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+int main(){
+
+  umask(0);
+  int fd1 = open("foo", O_CREAT | O_RDWR, 0666);
+  int fd2 = dup(fd1);
+  pid_t pid = fork();
+
+  if(pid == 0){
+    write(fd2, "Hi, Mom", 7);
+    close(fd2);
+    return 0;
+  }
+
+  wait(NULL);
+  lseek(fd1, SEEK_SET,0);
+  char rbuf[7]; //We are making our buffer 7 since we know exactly how much we're writing.
+  read(fd1, rbuf, 7);
+  printf("My son said %s\n", rbuf);
+  close(fd1);
+
+  return 0;
+
+}
